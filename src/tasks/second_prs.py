f56cd0e44427f8207a807f29c4516da01589fb0f
Ddilibe <franklinfidelugwuowo@gmail.com>
2026-07-16 14:03:12 +0100

    Feat: added second task for PRS certificate generation

---
+++ src/tasks/second_prs.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+import hmac
+import uuid
+import string
+import random
+import hashlib
+from pathlib import Path
+from typing import Callable
+
+from celery import Celery
+from decouple import config
+from PIL import ImageFont, Image, ImageDraw
+from celery.utils.log import get_task_logger
+
+from src.custom_email import ZohoEmailer
+from src.qr_generator import add_qr_to_template, generate_qr_image
+from src.template import EmailTemplate
+from src.api_caller import create_certification
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+logger = get_task_logger(__name__)
+
+email_username = str(config("EMAIL_USERNAME"))
+email_password = str(config("EMAIL_PASSWORD"))
+
+
+def custom_task(data: dict[str, str]) -> str:
+    person_name = data.get("name")
+    email = data.get("email")
+
+    key = str(config("CERT_AUTH_KEY", "CERT_AUTH_KEY")).encode()
+    raw_string = "".join([str(uuid.uuid4()) for _ in range(2)]).replace(
+        "-", random.choice(list(string.ascii_letters))
+    )
+    first_10_chars = raw_string[:10]
+    message = first_10_chars.encode()
+
+    hmac_key = hmac.new(key, message, hashlib.sha256).hexdigest()
+
+    if not person_name:
+        raise ValueError("Missing required field: 'name'")
+    if not email:
+        raise ValueError("Missing required field: 'email'")
+
+    name_length = len(person_name)
+    if name_length <= 30:
+        font_size = 100
+    elif name_length <= 40:
+        font_size = 90
+    elif name_length <= 50:
+        font_size = 80
+    else:
+        font_size = 70
+
+    try:
+        font = ImageFont.truetype(
+            BASE_DIR / Path("assets/fonts/Birthstone-Regular.ttf"), font_size
+        )
+    except OSError:
+
+        font = ImageFont.load_default(font_size)
+        print("Warning: arial.ttf not found, using default font.")
+
+    # text_position = (130, 600)
+    template_path = BASE_DIR / Path("assets/media/second_prs_certificate.png")
+    if not template_path.exists():
+        raise FileNotFoundError(
+            "Certificate template not found: certificate_template.png",
+            f"{template_path}",
+        )
+
+    with Image.open(template_path) as img:
+        img = img.convert("RGB")
+        draw = ImageDraw.Draw(img)
+        stroke_width = 1
+        stroke_fill = "#1A693D"
+
+        img_w, _ = img.size
+        bbox = draw.textbbox((0, 0), person_name.strip().upper(), font=font)
+        text_width, _ = bbox[2] - bbox[0], bbox[3] - bbox[1]
+
+        x = (img_w - text_width) / 2 - bbox[0]
+
+        draw.text(
+            (x, 570),
+            person_name.strip().upper(),
+            font=font,
+            fill="#1A693D",
+            stroke_width=stroke_width,
+            stroke_fill=stroke_fill,
+            # anchor="mm",
+        )
+        if img.mode in ("RGBA", "P"):
+            img = img.convert("RGB")
+
+        certificate_url = str(config("CERTIFICATE_URL"))
+        url = f"{certificate_url}?key={message.decode()}&hmac={hmac_key}"
+
+        qr = generate_qr_image(url)
+        add_qr_to_template(img, qr, 920, 1010, (200, 200))
+
+        output_path = Path(
+            f"pdf_uploads/second_prs_2026/{person_name.replace(' ', '_')}_certificate.pdf"
+        )
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        img.save(output_path, "PDF", resolution=100.0)
+
+        mailer = ZohoEmailer(email_username, email_password)
+        a = person_name.split(" ")
+
+        email_name = a[0]
+        mailer.send_email(
+            str(email),
+            "CISON 2026 Q2 PRS Certificate",
+            html_body=EmailTemplate.second_prs_template(email_name),
+            attachments=[str(output_path)],
+        )
+
+    create_certification(
+        f"2026 Q2 PRS Certificate",
+        output_path,
+        message,
+        hmac_key,
+        data.get("name"),
+        data.get("email"),
+    )
+
+    return str(output_path)
+
+
+def create_second_prs_2026_certificate(app: Celery) -> Callable:
+
+    logger = get_task_logger(__name__)
+    logger.info("Second PRS Task Created")
+
+    @app.task(
+        bind=True,
+        name="certification.first_second_2026",
+        autoretry_for=(Exception,),
+        retry_backoff=True,
+        retry_jitter=True,
+        max_retries=5,
+        acks_late=True,
+        reject_on_worker_lost=True,
+    )
+    def create_certificates(self, **kwargs):
+        logger.info("Performing Task")
+        logger.info(f"Creating PRS Certificates for: {kwargs.get('name')}")
+        try:
+            custom_task(kwargs)
+            logger.info(f"Certificate created successfully for {kwargs.get('name')}")
+        except Exception as e:
+            logger.error(f"Failed for {kwargs.get('name')}: {e}", exc_info=True)
+            raise
+
+    return create_certificates
